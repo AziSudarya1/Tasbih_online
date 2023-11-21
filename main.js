e2335432de8f65e4bf8125bd80b98adf2021b5f7
AziSudarya1 <azisudarya98@gmail.com>
2023-11-21 21:10:07 +0700

    fix pt. 1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function welcome() {
-  alert("Selmat datang di website tasbih online by Azi :)");
+  alert("Selmat datang di website tasbih online tetaplah berbuat kebaikan :)");
   const removeHidden = document.querySelector("#contents");
   removeHidden.removeAttribute("hidden");
 }
@@ -8,10 +8,6 @@ document.body.onload = welcome;
 
 function increment(){
   const increment = document.querySelector("#count").innerText++;
-      if(increment >= 99){
-        const getNewElmeent = document.getElementById('muncul');
-        getNewElmeent.innerText = 'hamdalah';
-      }
     }
 document.getElementById("incrementButton").onclick = increment;
 function clear() {
